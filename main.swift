6a94309996346a6910b4fd0d8b23e4590638d31c
stevenprichard <steven.prichard@boldcommerce.com>
2017-10-10 22:39:28 -0500

    init commit

---
+++ main.swift
@@ -0,0 +1,76 @@
+protocol Domain {
+    var tasks : [Task] {get set}
+    var name : String {get set}
+    var timeSpent : Int {get}
+}
+
+protocol Task {
+    var name : String {get set}
+    var duration : Int {get set}
+}
+
+
+
+struct Education : Domain{
+    var name : String
+    var tasks: [Task]
+    var timeSpent: Int {
+        var sum = 0
+        for task in tasks{
+            sum += task.duration
+        }
+        return sum
+    }
+    
+    init(name: String = "Education", tasks: [Task] = []) {
+        self.name = name
+        self.tasks = tasks
+    }
+}
+
+struct Study : Task{
+    var name: String
+    var duration: Int
+    
+    init(name: String = "Study", duration: Int) {
+        self.name = name
+        self.duration = duration
+    }
+}
+
+struct Homework : Task {
+    var name: String
+    var duration: Int
+    
+    init(name: String = "Homework", duration: Int) {
+        self.name = name
+        self.duration = duration
+    }
+}
+
+struct GeneralTask : Task {
+    var name: String
+    var duration: Int
+    
+    init(name: String, duration: Int) {
+        self.name = name
+        self.duration = duration
+    }
+}
+
+
+
+var study = Study(duration: 30)
+var hw = Homework(duration: 60)
+
+var masters = Education(tasks: [study, hw])
+masters.tasks
+masters.timeSpent
+
+var party = GeneralTask(name: "Party", duration: 10000)
+var underGrad = Education(name: "Under Grad", tasks: [study, hw, party])
+underGrad.name
+underGrad.tasks
+underGrad.timeSpent
+
+
